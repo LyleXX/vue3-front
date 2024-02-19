26734d62062edaacd258ff9e5a1da8e550082cb2
lyleXX <740334078@qq.com>
2024-02-19 22:31:26 +0800

    finish section 6

---
+++ src/libs/popup/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <teleport to="body">
+      <transition name="fade">
+        <div
+          v-if="isVisible"
+          class="w-screen h-screen bg-zinc-900/80 z-40 fixed top-0 left-0"
+          @click="isVisible = false"
+        ></div>
+      </transition>
+      <transition name="popup-down-up">
+        <div
+          v-if="isVisible"
+          v-bind="$attrs"
+          class="w-screen bg-white z-50 fixed bottom-0"
+        >
+          <slot />
+        </div>
+      </transition>
+    </teleport>
+  </div>
+</template>
+
+<script setup>
+import { useScrollLock } from '@vueuse/core';
+import { watch } from 'vue';
+
+const isVisible = defineModel({ required: true });
+
+const isLocked = useScrollLock(document.body);
+
+watch(
+  isVisible,
+  (val) => {
+    isLocked.value = val;
+  },
+  {
+    immediate: true
+  }
+);
+</script>
+
+<style lang="scss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.3s;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.popup-down-up-enter-active,
+.popup-down-up-leave-active {
+  transition: all 0.3s;
+}
+
+.popup-down-up-enter-from,
+.popup-down-up-leave-to {
+  transform: translateY(100%);
+}
+</style>
--- src/views/main/navigation/mobile/index.vue
+++ src/views/main/navigation/mobile/index.vue
@@ -2,33 +2,90 @@
   <div class="bg-white sticky top-0 left-0 z-10">
     <ul
       class="relative flex overflow-x-auto p-1 text-xs text-zinc-600 overflow-hidden"
+      ref="ulTarget"
     >
+      <!-- 滑块 -->
+      <li
+        class="absolute h-[22px] bg-zinc-900 rounded-lg duration-200"
+        :style="sliderStyle"
+      ></li>
+
       <!-- 汉堡按钮 -->
       <li
         class="z-20 fixed top-0 right-[-1px] h-4 px-1 flex items-center bg-white shadow-l-white"
+        @click="isOpenPopup = !isOpenPopup"
       >
-        <m-svg-icon class="w-1.5 h-1.5" name="hamburger"></m-svg-icon>
+        <m-svg-icon class="w-1.5 h-1.5" name="hamburger" />
       </li>
 
       <!-- category item -->
       <li
-        v-for="item in data"
+        v-for="(item, index) in data"
         :key="item.id"
         class="shrink-0 px-1.5 py-0.5 z-10 duration-200 last:mr-4"
+        :ref="setItemRef"
+        :class="{
+          'text-zinc-100': index === currentCategoryIndex
+        }"
+        @click="changeCurrentCategoryIndex(index)"
       >
         {{ item.name }}
       </li>
     </ul>
+    <m-popup v-model="isOpenPopup">
+      <menu-vue :categorys="data" @onItemClick="onItemClick"></menu-vue>
+    </m-popup>
   </div>
 </template>
 
 <script setup>
+import menuVue from '@/views/main/components/menu/index.vue';
+import { onBeforeUpdate, ref, watch } from 'vue';
+import { useScroll } from '@vueuse/core';
+
 defineProps({
   data: {
     type: Array,
     required: true
   }
 });
+
+const ulTarget = ref(null);
+const sliderStyle = ref({
+  width: '52px',
+  transform: 'translateX(0px)'
+});
+
+const currentCategoryIndex = ref(0);
+const { x: ulScrollLeft } = useScroll(ulTarget);
+const changeCurrentCategoryIndex = (index) => {
+  currentCategoryIndex.value = index;
+};
+
+let itemRefs = [];
+const setItemRef = (el) => {
+  if (el) {
+    itemRefs.push(el);
+  }
+};
+onBeforeUpdate(() => {
+  itemRefs = [];
+});
+watch(currentCategoryIndex, (val) => {
+  const { left, width } = itemRefs[val].getBoundingClientRect();
+  sliderStyle.value = {
+    width: `${width}px`,
+    transform: `translateX(${ulScrollLeft.value + left - 10}px)`
+  };
+});
+
+const isOpenPopup = ref(false);
+
+// item 点击事件
+const onItemClick = (index) => {
+  currentCategoryIndex.value = index;
+  isOpenPopup.value = false;
+};
 </script>
 
 <style lang="scss" scoped></style>
